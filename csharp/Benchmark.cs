f23d76ee64e08b5734b1333ba5a95835ac220576
Parth Patil <parthvin@gmail.com>
2024-04-12 16:10:25 -0400

    Added support for C#

--- csharp/Benchmark.cs
+++ csharp/Benchmark.cs
@@ -7,23 +7,27 @@ class Benchmark
 {
     static void Main(string[] args)
     {
-        if (args.Length != 1)
+        if (args.Length <= 1)
         {
-            Console.WriteLine("Usage: benchmark <filename>");
+            Console.WriteLine("Usage: benchmark <filename> regex1 [regex2 ...]");
             Environment.Exit(1);
         }
 
         StreamReader reader = new System.IO.StreamReader(args[0]);
         string data = reader.ReadToEnd();
 
-        // Email
-        Benchmark.Measure(data, @"[\w\.+-]+@[\w\.-]+\.[\w\.-]+");
+        for (int i = 1; i < args.Length; i++)
+        {
+            Benchmark.Measure(data, args[i]);
+        }
+        // // Email
+        // Benchmark.Measure(data, @"[\w\.+-]+@[\w\.-]+\.[\w\.-]+");
 
-        // URI
-        Benchmark.Measure(data, @"[\w]+://[^/\s?#]+[^\s?#]+(?:\?[^\s#]*)?(?:#[^\s]*)?");
+        // // URI
+        // Benchmark.Measure(data, @"[\w]+://[^/\s?#]+[^\s?#]+(?:\?[^\s#]*)?(?:#[^\s]*)?");
 
-        // IP
-        Benchmark.Measure(data, @"(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9])");
+        // // IP
+        // Benchmark.Measure(data, @"(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9])");
     }
 
     static void Measure(string data, string pattern)
